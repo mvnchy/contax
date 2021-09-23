26affa451b9583f6774402c36e61fc9565ac17e3
Jose Raf Santos <jose.santos0326@gmail.com>
2021-09-22 21:28:11 -0400

    comment fix

--- db.js
+++ db.js
@@ -29,7 +29,7 @@ const db = getFirestore();
 
 
 
-/********************  Add component ***********************/
+/********************  A D D  component ***********************/
 
 const Add = {
 
@@ -104,7 +104,7 @@ const Add = {
   `
 }
 
-/********************  Info card ***********************/
+/********************  I N F O  card ***********************/
 
 const Contact = {
   props: ['docId'],
@@ -191,7 +191,7 @@ const Contact = {
   
   `
 }
-/********************  Home component ***********************/
+/********************  H O M E   component ***********************/
 
 const Home = {
 
@@ -270,11 +270,12 @@ const Home = {
 
 }
 
-/********************  Edit component  ***********************/
+/********************  E D I T   component  ***********************/
 
 const Edit = {
   props: ['docId'],
   data () {
+
     return {
       contax: {
         first: '',
@@ -285,13 +286,18 @@ const Edit = {
       }
     }
   },
+  methods: {
+    async uppity () {
+      console.log('connection secured')
+    }
+  },
+
   mounted: async function () {
+
     console.log(this.docId)
     const snap = await getDoc(doc(db, 'contacts', this.docId)).then(waka => {
 
       if (waka.exists) {
-        // console.log(wakla.data())
-
         this.contax.first = waka.data().firstName
         this.contax.last = waka.data().lastName
         this.contax.email = waka.data().email
@@ -299,69 +305,63 @@ const Edit = {
         this.contax.address = waka.data().address
       }
     })
-
-
-
-
   },
-
-
   template: `
 
-  <section class=" mt-5 row">
+    <section class=" mt-5 row">
 
-  <div class="row">
-            <div class="col-6 display-6">Edit Contact</div>
-            <div class="col-6 text-end align-self-center">
-              <router-link style="text-decoration: none;" to='/'>Save</router-link>
-            </div>
-  </div>
+    <div class="row">
+              <div class="col-6 display-6">Edit Contact</div>
+              <div class="col-6 text-end align-self-center">
+                <router-link @click='uppity' style="text-decoration: none;" to='/'>Save</router-link>
+              </div>
+    </div>
 
 
-<!-------------------- first + last name       -->
-    <div class="row col-12">
+  <!-------------------- first + last name       -->
+      <div class="row col-12">
+
+          <div class="col-6">
+            <input type="text" v-model='contax.first' class='form-control my-2 '>
+          </div>
+
+          <div class="col-6">
+            <input type="text" v-model='contax.last' class='form-control my-2'>
+          </div>
+
+      </div>
+  <!-------------------- email + phoneNUmber      -->
+      <div class="row col-12" >
 
         <div class="col-6">
-          <input type="text" v-model='contax.first' class='form-control my-2 '>
+          <input type='email' v-model='contax.email' class='form-control my-2 '>
         </div>
-
         <div class="col-6">
-          <input type="text" v-model='contax.last' class='form-control my-2'>
+          <input type='tel' v-model='contax.phone' class="form-control my-2 ">
         </div>
 
-    </div>
-<!-------------------- email + phoneNUmber      -->
-    <div class="row col-12" >
-
-      <div class="col-6">
-        <input type='email' v-model='contax.email' class='form-control my-2 '>
-      </div>
-      <div class="col-6">
-        <input type='tel' v-model='contax.phone' class="form-control my-2 ">
       </div>
 
-    </div>
+      <div class="row col-12">
+        <div class="col">
+          <input type="text" v-model='contax.address' class="col my-2 form-control ">
+        </div>
+      </div> 
+    
 
-    <div class="row col-12">
-      <div class="col">
-         <input type="text" v-model='contax.address' class="col my-2 form-control ">
+      <div class="row col-12">
+          <router-link to='/' class="col">
+            <button class="btn btn-secondary form-control">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                  </svg>
+            </button>
+          </router-link>
       </div>
-    </div> 
-  
-
-    <div class="row col-12">
-        <router-link to='/' class="col">
-           <button class="btn btn-secondary form-control">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                          <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                </svg>
-          </button>
-        </router-link>
-    </div>
 
 
-</section>
+  </section>
 
   `
 
